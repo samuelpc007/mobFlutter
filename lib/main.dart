ea073c5b11f61ae7b36523f0980fd84b3dab5277
Samuel Pereira Costa <46694833+samuelmfa@users.noreply.github.com>
2020-02-08 07:47:26 -0300

    Implementação Classes

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(BytebankApp());
 
+/* Corpo Principal */
 class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,6 +13,7 @@ class BytebankApp extends StatelessWidget {
   }
 }
 
+/* Lista Tranferencia */
 class FormularioTransferencia extends StatelessWidget {
   final TextEditingController _controladorCampoNumeroConta =
       TextEditingController();
@@ -25,56 +27,30 @@ class FormularioTransferencia extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: TextField(
-              controller: _controladorCampoNumeroConta,
-              style: TextStyle(
-                fontSize: 18.0,
-              ),
-              obscureText: false,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  icon: Icon(Icons.account_balance),
-                  labelText: 'Numero da Conta',
-                  hintText: '0000'),
-              keyboardType: TextInputType.number,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: TextField(
-              controller: _controladorCampoValor,
-              style: TextStyle(
-                fontSize: 18.0,
-              ),
-              obscureText: false,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  icon: Icon(Icons.monetization_on),
-                  labelText: 'Valor',
-                  hintText: '0.00'),
-              keyboardType: TextInputType.number,
-            ),
-          ),
+          Editor(controlador: _controladorCampoNumeroConta, rotulo: 'Numero da Conta', dica:'0000', icone: Icons.account_balance),
+          Editor(controlador: _controladorCampoValor, rotulo: 'Valor', dica:'0.00', icone: Icons.monetization_on),                      
           RaisedButton(
-            onPressed: () {
-              final int numeroConta =
-                  int.tryParse(_controladorCampoNumeroConta.text);
-              final double valor = double.tryParse(_controladorCampoValor.text);
-              if (numeroConta != null && valor != null) {
-               final tranferenciaCriada = Transferencia(valor, numeroConta);
-               debugPrint('$tranferenciaCriada');
-              }
-            },
+            onPressed: () => _criaTransferencia(),
             child: Text('Confirmar', style: TextStyle(fontSize: 16)),
           ),
         ],
       ),
     );
   }
+  
 }
 
+void _criaTransferencia(){
+    final int numeroConta =
+                  int.tryParse(_controladorCampoNumeroConta.text);
+              final double valor = double.tryParse(_controladorCampoValor.text);
+              if (numeroConta != null && valor != null) {
+                final tranferenciaCriada = Transferencia(valor, numeroConta);
+                debugPrint('$tranferenciaCriada');
+              }
+  }
+
+/* Lista Tranferencia */
 class ListaTransferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -117,8 +93,38 @@ class Transferencia {
 
   Transferencia(this.valor, this.numeroConta);
 
-  @override 
-  String toString(){
+  @override
+  String toString() {
     return 'Tranferencia: {valor: $valor, numeroConta: $numeroConta}';
+  }
+}
+
+class Editor extends StatelessWidget {
+
+  final TextEditingController controlador;
+  final String rotulo;
+  final String dica;
+  final IconData icone;
+
+  Editor({this.controlador, this.rotulo, this.dica, this.icone});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: TextField(
+        controller: controlador,
+        style: TextStyle(
+          fontSize: 18.0,
+        ),
+        obscureText: false,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            icon: icone != null ? Icon(icone) : null,
+            labelText: rotulo,
+            hintText: dica),
+        keyboardType: TextInputType.number,
+      ),
+    );
   }
 }
